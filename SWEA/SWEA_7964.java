1b0dbc9a3b4875ddee368ac49af207025a77707e
ChaerinYu <chaerin.du.ub@gmail.com>
2021-08-26 22:36:32 +0900

    210826 algorithm

--- SWEA/SWEA_7964.java
+++ SWEA/SWEA_7964.java
@@ -2,6 +2,7 @@ package SWEA;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 /**
  * [D3] 7964. 부먹왕국의 차원 관문
@@ -9,6 +10,39 @@ import java.io.InputStreamReader;
 public class SWEA_7964 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
 		
+		StringTokenizer st = null;
+		for (int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int n = Integer.parseInt(st.nextToken()); // 왕국 도시의 수
+			int dist = Integer.parseInt(st.nextToken()); // 제한 거리
+			
+			int[] cities = new int[n]; // 도시 지도 정보
+			// 입력
+			st = new StringTokenizer(br.readLine(), " ");
+			for (int i = 0; i < n; i++) {
+				cities[i] = Integer.parseInt(st.nextToken());
+			}
+			
+			int buildingIdx = 0, limit = 0;
+			int res = 0;
+			for (int i = 0; i < n; i++) {
+				if(cities[i] == 1) {
+					buildingIdx = i; // 최신으로 남아있는 인덱스 업데이트
+					limit = 0;
+				} else {
+					limit++;
+					// 제한거리 꽉 차면 도시 새로 세워준다.
+					if(limit == dist) {
+						limit = 0; // 제한 초기화
+						buildingIdx = i; // 빌딩 세우기
+						res++;
+					}
+				}
+			}
+			
+			System.out.println("#"+tc+" "+res);
+		}
 	}
 }
--- jungol/jo_1205.java
+++ jungol/jo_1205.java
@@ -2,9 +2,9 @@ package jungol;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 /** TODO
@@ -14,7 +14,7 @@ import java.util.StringTokenizer;
  * 조커 카드와 일반 카드가 주어졌을 때 가장 긴 길이의 스트레이트를 만드는 프로그램을 작성하라.
  */
 public class jo_1205 {
-
+	static PriorityQueue<Integer> cards = new PriorityQueue<Integer>();
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -24,16 +24,17 @@ public class jo_1205 {
 		
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 //		int[] cards = new int[N];
-		LinkedList<Integer> cards = new LinkedList<Integer>();
+//		LinkedList<Integer> cards = new LinkedList<Integer>();
 		int jokerNum = 0;
 		int minNum = Integer.MAX_VALUE; // 조커를 제외한 가장 작은 숫자
 		for (int i = 0; i < N; i++) {
-			cards.add(Integer.parseInt(st.nextToken())); // 카드 값 입력
-			if(cards.get(i) == 0)
+			int num = Integer.parseInt(st.nextToken());
+			cards.offer(num); // 카드 값 입력
+			if(num == 0)
 				jokerNum++;
 			
-			if(minNum>cards.get(i)) {
-				minNum = cards.get(i);
+			if(minNum>num) {
+				minNum = num;
 			}
 //			cards[i] = Integer.parseInt(st.nextToken()); // 카드 값 입력
 //			if(cards[i] == 0)
@@ -45,21 +46,26 @@ public class jo_1205 {
 		}
 		
 //		Arrays.sort(cards); // 오름차순 정렬
-		Collections.sort(cards); // 오름차순 정렬
+//		Collections.sort(cards); // 오름차순 정렬
 		
 //		boolean hasJoker = cards[0]==0?true:false;
 		
 		int res = 0;
 //		int maxNum = cards[N-1]; // 가장 큰 숫자
-		int maxNum = cards.get(N-1); // 가장 큰 숫자
+		int maxNum = cards.peek(); // 가장 큰 숫자
 		
 		// 조커 없는 경우
 		if(jokerNum == 0) {
 			int maxLen = 1;
-			for (int i = 1; i < N; i++) {
-				if(cards.get(i-1)+1==cards.get(i)) maxLen++;
+			int firstMax = cards.poll();
+			while(!cards.isEmpty()) {
+				int secondMax = cards.poll();
+				// queue poll 하고 난 뒤 또 poll 해서 비교
+				if(firstMax+1==secondMax) maxLen++;
 				else maxLen = 1;
 				res = Math.max(res, maxLen);
+				
+				firstMax = secondMax;
 			}
 			System.out.println(res);
 			return;
@@ -68,49 +74,39 @@ public class jo_1205 {
 		int maxLen = 1;
 		int tempNum = jokerNum;
 		int i = 1;
-		while(i<N) {
-//		for (int i = jokerNum; i < N; i++) {
-			if(cards.get(i-1)+1==cards.get(i)) {
-				maxLen++;
-				i++;
-			}
+		while(!cards.isEmpty() && cards.peek() == 0) {
+			cards.poll();
+		}
+		
+		int firstMax = cards.poll();
+		while(!cards.isEmpty()) {
+			int secondMax = cards.poll();
+			System.out.println("ff"+firstMax);
+			System.out.println("ss"+secondMax);
+			if(firstMax+1 == secondMax) maxLen++;
 			else {
-				if(tempNum>0 && cards.get(i-1)+tempNum+1>=cards.get(i)) {
-					int temp = 0;
-					while(temp<=tempNum && cards.get(i-1)+temp+1<cards.get(i)) {
+				System.out.println("Aa");
+				if(firstMax+1+tempNum>=secondMax) {
+					while(tempNum>=0 && firstMax+1+tempNum>secondMax) {
+						System.out.println("while:"+tempNum);
+						tempNum--;
 						maxLen++;
-//						tempNum--;
-						temp++;
+						firstMax = secondMax;
 					}
-					tempNum-=temp;
+					maxLen+=tempNum+1;
+					System.out.println("remain: "+tempNum);
+					continue;
+//					continue;
 				} else {
+					tempNum = jokerNum;
 					maxLen = 1;
-					tempNum=jokerNum;
 				}
 			}
 			res = Math.max(res, maxLen);
+			firstMax = secondMax;
 		}
 		System.out.println(res);
 		return;
-			
-//			if(cards[i-1]!=0 && cards[i-1]+1==cards[i])
-			
-//			if(cards[i-1]==0 || cards[i-1]+1==cards[i]) {
-//				res++;
-//			} else {
-//				if(tempJokerNum>0 && i+1<N && cards[i-1]+2==cards[i+1]) {
-//					// 조커 발동
-////					res++;
-//					tempJokerNum--;
-//					continue;
-//				} else {
-//					tempJokerNum = jokerNum;
-//					res = 0;
-//				}
-//			}
-		
-		
-//		System.out.println(res);
 		
 	}
 }
