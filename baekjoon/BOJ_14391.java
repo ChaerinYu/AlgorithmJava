5c1845fb227644bfe90fae301af3eb7ecfcde126
ChaerinYu <chaerin.du.ub@gmail.com>
2021-09-18 23:45:58 +0900

    210918 algorithm

--- baekjoon/BOJ_14391.java
+++ baekjoon/BOJ_14391.java
@@ -15,8 +15,11 @@ import java.util.StringTokenizer;
  */
 public class BOJ_14391 {
 
+	static final int[][] delta = {{1, 0}, {0, 1}};
 	static int N, M; // 세로, 가로  (1 ≤ N, M ≤ 4)
 	static int[][] paper; // 종이
+	
+	static boolean[][] visited;
 	
 	static int res; // 정답
 	
@@ -36,9 +39,57 @@ public class BOJ_14391 {
 				paper[r][c] = charArr[c]-'0';
 			}
 		}
+		
+		visited = new boolean[N][M];
 		res = 0;
+		dfs(0);
 		
 		System.out.println(res);
+	}
+	
+	static void dfs(int idx) {
+		if(idx == N*M) {
+			int sum = 0, temp = 0;
+			// 가로로 자르기
+			for (int r = 0; r < N; r++) {
+				temp = 0;
+				for (int c = 0; c < M; c++) {
+					// 잘림 (하나의 집합)
+					if(visited[r][c]) {
+						temp = temp*10 + paper[r][c];
+					} else {
+						// 새로운 종이조각 시작 필요 
+						sum += temp;
+						temp = 0;
+					}
+				}
+				sum += temp;
+			}
+			// 가로로 안자르고 남은 종이들 세로로 자르기
+			for (int c = 0; c < M; c++) {
+				temp = 0;
+				for (int r = 0; r < N; r++) {
+					// 안잘림
+					if(!visited[r][c]) {
+						temp = temp*10 + paper[r][c];
+					} else {
+						sum += temp;
+						temp = 0;
+					}
+				}
+				sum += temp;
+			}
+			res = Math.max(res, sum);
+			return;
+		}
+		
+		int r = idx/N;
+		int c = idx%M;
+		
+		visited[r][c] = true; // 가로 자르기
+		dfs(idx+1);
+		visited[r][c] = false; // 세로 자르기
+		dfs(idx+1);
 	}
 	
 	private static String src = "4 3\r\n" + 
